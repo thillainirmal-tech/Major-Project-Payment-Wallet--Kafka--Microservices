342f44fdf0b294e9261a5ef661f8ca547007d3e6
Thillai Nirmal K <120569119+thillainirmal-tech@users.noreply.github.com>
2025-09-28 11:18:54 +0530

    Add files via upload

---
+++ transaction.sql
@@ -0,0 +1,4 @@
+id,amount,comment,date_created,from_user_id,last_updated,reason,status,to_user_id,txn_id
+1,30,testing,"2025-09-27 11:02:06.572383",7,"2025-09-27 11:02:07.525142","Wallet not found",FAILED,10,eb898eb7-c334-47bd-b11c-76f31efa1395
+2,30,rent,"2025-09-27 11:08:04.039790",11,"2025-09-27 11:08:04.290940",NULL,SUCCESS,12,8d15f219-44f4-46ce-b9d9-2986af51ec36
+3,30,testing,"2025-09-27 12:39:18.971293",11,"2025-09-27 12:39:19.718683",NULL,SUCCESS,12,59f64aac-7042-4668-84cb-ca7bc63b091c
